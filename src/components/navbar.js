6cf633b892097c6ad08cdae7e2c37529d5a58d06
Rahul Senthil Kumar <rahulsk4087@gmail.com>
2024-10-05 02:47:54 +0530

    navbar v1

---
+++ src/components/navbar.js
@@ -0,0 +1,18 @@
+
+export default function Navbar(){
+
+    return(
+        <>
+        <div className="main-div">
+            <div className="sub-div">
+                <h1 className="heading">App Name</h1>
+                <button>Hello</button>
+                <button>HITHERE</button>
+                <button>Rampkryt</button>
+            </div>
+            <button>Login</button>
+            <button>Sign Up</button>
+        </div>
+        </>
+    )
+}
